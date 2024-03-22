a240487d015515315650fc53676862b330782588
Maks <unreal9981@gmail.com>
2024-03-23 00:41:55 +0200

    wip

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Locale } from '@/i18n-config';
 
 import { getDictionary } from '@/get-dictionary';
-import Header from '@/shared/header/header';
-import Footer from '@/shared/footer/footer';
+import Header from '@/shared/header/Header';
+import Footer from '@/shared/footer/Footer';
 import AboutPage from './components/about';
 
 async function About({ params: { lang } }: { params: { lang: Locale } }) {
--- src/app/[lang]/shop/page.tsx
+++ src/app/[lang]/shop/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Locale } from '@/i18n-config';
 
 import { getDictionary } from '@/get-dictionary';
-import Header from '@/shared/header/header';
-import Footer from '@/shared/footer/footer';
+import Header from '@/shared/header/Header';
+import Footer from '@/shared/footer/Footer';
 
 async function Shop({ params: { lang } }: { params: { lang: Locale } }) {
   const dictionary = await getDictionary(lang);
