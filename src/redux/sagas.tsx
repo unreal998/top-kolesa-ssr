f0ce9232347f834f27da445a97d9b825d0563d7e
Maks <unreal9981@gmail.com>
2024-04-01 14:45:32 +0300

    wip

--- src/redux/sagas.tsx
+++ src/redux/sagas.tsx
@@ -1,7 +1,9 @@
 import { all } from 'redux-saga/effects';
 import { watchFilterData } from './sagas/filterDataSaga';
 import { watchShopGetItems } from './sagas/shopDataSaga';
+import { watchAdressByInputSaga } from './sagas/checkoutDataSaga';
+import { watchOrderData } from './sagas/orderDataSaga';
 
 export function* rootSaga() {
-  yield all([watchFilterData(), watchShopGetItems()]);
+  yield all([watchFilterData(), watchShopGetItems(), watchAdressByInputSaga(), watchOrderData()]);
 }
