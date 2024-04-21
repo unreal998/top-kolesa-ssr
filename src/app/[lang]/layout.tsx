ad97901995117c1c53cda6eba5b099037e5d159c
Romchik Stakhiv <Roma.Stakhiv@gmail.com>
2024-04-21 21:05:09 +0300

    finalVersionFavIcon

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -27,9 +27,6 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang={params.lang} className={ptSans.className}>
-        {/* <head>
-          <link rel="icon" href="/favicon.ico" />
-        </head> */}
         <body>
           <main>{children}</main>
         </body>
