6ff831031b0586f3ff37300358e1f8962cbede1e
Maks <unreal9981@gmail.com>
2024-05-11 00:27:04 +0300

    wip

--- src/shared/keys.tsx
+++ src/shared/keys.tsx
@@ -6,4 +6,4 @@ export const NOVA_POSHTA_API = 'https://api.novaposhta.ua/v2.0/json/';
 export const API_KEY = 'a092079f1673b334914ab2ab539e84c6';
 export const SHOP_ITEM_TIRES_IMG_PREFIX =
   'https://topkolesa.com.ua/uploads/autosize/tires/size/350x350x2/2/f/';
-export const GOOGLE_MAP_KEY = 'AIzaSyAcevIicxm5rS4gGwZh9-UBarx9Z5Rel9U';
+export const GOOGLE_MAP_KEY = 'AIzaSyAYKnoPSFS1oyGQ94Y3beD5Dc-YUopGAPs';
